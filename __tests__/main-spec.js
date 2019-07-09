3fbd3ef26ef15e7a84ef661a8d44da4f087d248e
linguangxiong <854501715@qq.com>
2019-07-10 01:08:15 +0800

    complete all project

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -2,6 +2,79 @@ const mainMethod = require('../main');
 
 //---------isBarCodeExist---Test------------------//
 
-function isBarCodeExist(){
 
-}
+var itemDatas = [
+    {"id": "0001", "name" : "Coca Cola", "price": 3},
+    {"id": "0002", "name" : "Diet Coke", "price": 4},
+    {"id": "0003", "name" : "Pepsi-Cola", "price": 5},
+    {"id": "0004", "name" : "Mountain Dew", "price": 6},
+    {"id": "0005", "name" : "Dr Pepper", "price": 7},
+    {"id": "0006", "name" : "Sprite", "price": 8},
+    {"id": "0007", "name" : "Diet Pepsi", "price": 9},
+    {"id": "0008", "name" : "Diet Mountain Dew", "price": 10},
+    {"id": "0009", "name" : "Diet Dr Pepper", "price": 11},
+    {"id": "0010", "name" : "Fanta", "price": 12}
+];
+
+
+var barcodes1 = ['0001', '0003', '0005', '0003'];
+var barcodes2 = [];
+var barcodes3 = ['0001', '0003', '0105', '0003'];
+
+
+var receipts = "Receipts\n" +
+    "------------------------------------------------------------\n" +
+    "Coca Cola  3  1\n" +
+    "Pepsi-Cola  5  2\n" +
+    "Dr Pepper  7  1\n" +
+    "------------------------------------------------------------\n" +
+    "Price: 20";
+
+var errorResult = "[ERROR]:";
+
+
+//---------isBarCodesExist--test-------//
+it ('should return true when call isBarCodesExist given barcodes1 and itemDatas', () => {
+    expect(mainMethod.isBarCodesExist(barcodes1, itemDatas)).toBe(true);
+});
+
+it ('should return false when call isBarCodesExist given barcodes2 and itemDatas', () => {
+    expect(mainMethod.isBarCodesExist(barcodes2, itemDatas)).toBe(false);
+});
+
+it ('should return false when call isBarCodesExist given barcodes3 and itemDatas', () => {
+    expect(mainMethod.isBarCodesExist(barcodes3, itemDatas)).toBe(false);
+});
+
+
+//---------caculateBarcodes--test-------//
+it ('should return result when call caculateBarcodes given barcodes1 and itemDatas', () => {
+    expect(mainMethod.caculateBarcodes(mainMethod.isBarCodesExist(barcodes1,itemDatas),barcodes1, itemDatas)).toBe(receipts);
+});
+
+
+it ('should return "[ERROR]:" when call caculateBarcodes given barcodes2 and itemDatas', () => {
+    expect(mainMethod.caculateBarcodes(mainMethod.isBarCodesExist(barcodes2,itemDatas),barcodes2, itemDatas)).toBe(errorResult);
+});
+
+it ('should return "[ERROR]:" when call caculateBarcodes given barcodes3 and itemDatas', () => {
+    expect(mainMethod.caculateBarcodes(mainMethod.isBarCodesExist(barcodes3,itemDatas),barcodes3, itemDatas)).toBe(errorResult);
+});
+
+
+
+
+//---------printReceiptsByBarcodes--test-------//
+it ('should return result when call printReceiptsByBarcodes given barcodes1 and itemDatas', () => {
+    expect(mainMethod.printReceiptsByBarcodes(barcodes1,itemDatas)).toBe(receipts);
+});
+
+
+it ('should return "[ERROR]:" when call printReceiptsByBarcodes given barcodes2 and itemDatas', () => {
+    expect(mainMethod.printReceiptsByBarcodes(barcodes2,itemDatas)).toBe(errorResult);
+});
+
+it ('should return "[ERROR]:" when call printReceiptsByBarcodes given barcodes3 and itemDatas', () => {
+    expect(mainMethod.printReceiptsByBarcodes(barcodes3, itemDatas)).toBe(errorResult);
+});
+
--- main.js
+++ main.js
@@ -5,13 +5,18 @@ function isBarCodesExist(barcodes,itemDatas){
         return false;
     }
 
+    var judge = 0;
     for(let i = 0;i < barcodes.length;i++){
+        judge = 0;
         for(let j = 0;j < itemDatas.length;j++){
             if(barcodes[i] == itemDatas[j]["id"]){
-                continue;
+                judge = 1;
+                break;
             }
         }
-        return false;
+        if(judge == 0){
+            return false;
+        }
     }
 
     return true;
@@ -22,14 +27,71 @@ function isBarCodesExist(barcodes,itemDatas){
 
 
 function caculateBarcodes(isExist,barcodes,itemDatas){
+
+    var result = [];
+
     if(!isExist){
         return "[ERROR]:";
     }
+
     else{
-        var result = "Receipts"+"/n"+"------------------------------------------------------------"
-        
+
         for(let i = 0;i < barcodes.length;i++){
-            
+            for(let j = 0;j < itemDatas.length;j++){
+
+                if(barcodes[i] == itemDatas[j]["id"]){
+
+                    var index = -1;
+                    for(let k = 0;k < result.length;k++){
+                        if(barcodes[i] == result[k]["id"]){
+                            index = k;
+                            break;
+                        }
+                    }
+
+                    if(index >= 0){  //if exist,add 1 directly
+                        result[index]["amount"]++;
+                    }
+
+                    else{           //if not exist,need to create a new object
+                        var temp = itemDatas[j];
+                        temp["amount"] = 1;
+                        result.push(temp);
+                        break;
+                    }
+
+                }
+            }
         }
+
+        return getStringFromObjectArray(result);
+
     }
 }
+
+
+function getStringFromObjectArray(objectArray) {
+
+    var result = "Receipts"+"\n"+"------------------------------------------------------------\n";
+    var totalPrice = 0;
+    for(let i = 0;i < objectArray.length;i++){
+        result += objectArray[i]["name"]+"  "+ objectArray[i]["price"] +"  "+ objectArray[i]["amount"]+"\n";
+        totalPrice += objectArray[i]["price"] * objectArray[i]["amount"];
+    }
+    result += "------------------------------------------------------------\n"+"Price: "+totalPrice;
+    return result;
+
+}
+
+
+
+function printReceiptsByBarcodes(barcodes,itemDatas) {
+    var result = isBarCodesExist(barcodes,itemDatas);
+    return caculateBarcodes(result,barcodes,itemDatas);
+}
+
+module.exports = {
+    isBarCodesExist,
+    caculateBarcodes,
+    printReceiptsByBarcodes
+};
